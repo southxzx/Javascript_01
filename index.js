5c7b85d5745ed387f9edcdfac26515deefe78dbb
southxz <southxzx109@users.noreply.github.com>
2020-07-24 22:01:18 +0700

    brbb

--- index.js
+++ index.js
@@ -1,2 +1,25 @@
-let name = 'Mosh';
-console.log(name);
+// let people = {
+//     name: 'Nam',
+//     age: 21
+// };
+// people.name = 'Nam Đại Nhân';
+
+// let selection = 'name';
+// people[selection] = 'Nam 109';
+// console.log(people);
+
+// /* ARRAY */
+// let selectedColor = ['red','blue'];
+// selectedColor[2] = 'green';
+// selectedColor[3] = 11;
+// console.log(selectedColor.length);
+
+/* FUNCTION */
+function greet(name, lastname){
+    console.log('Hello ' + name + ' ' + lastname);
+}
+function square(number){
+    return number*number;
+}
+console.log((square(2)))
+// greet('Nam', 'Dang');
