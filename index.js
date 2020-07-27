91957b00369396c8482c5ac7c6f9e0079353639f
southxz <southxzx109@users.noreply.github.com>
2020-07-27 09:14:34 +0700

    already done Object

--- index.js
+++ index.js
@@ -138,12 +138,12 @@ const circle = {
 //     };
 // }
 // const circle = createCircle(1);
-// function Circle(radius){
-//     this.radius = radius;
-//     this.draw = function(){
-//         console.log('DRAW');
-//     }
-// }
+function Circle(radius){
+    this.radius = radius;
+    this.draw = function(){
+        console.log('DRAW');
+    }
+}
 // const circle = new Circle(1);
 // console.log(circle);
 // let obj = {value: 10};
@@ -152,6 +152,103 @@ const circle = {
 // }
 // increaseValue(obj);
 // console.log(obj);
-for (let x in circle){
-    console.log(x, circle[x]);
-};
+// for (let x in circle){
+//     console.log(x, circle[x]);
+// };
+// const another = Object.assign({},circle);
+// const another = {...circle};
+// console.log(another);
+// let name = 'NAM';
+// const message = 
+// `I'm a little
+// 'monster' ${name}`;
+
+const now = new Date();
+const date1 = new Date('July 27 2020 08:12');
+const date2 = new Date(2020, 07, 27, 08, 13);
+now.setFullYear(2021);
+
+//Ex1
+// const address = {
+//     street: 'Lê Văn Việt',
+//     city: 'HCMC',
+//     zipCode: 70000
+// };
+// function showAddress(address){
+//     const result =
+//     `Street: ${address['street']}
+//     City: ${address['city']}
+//     Zip Code: ${address['zipCode']}`;
+//     console.log(result);    
+// }
+// showAddress(address);
+
+//Ex2
+//Factory Function
+// function address(street,city,zipcode){
+//     return{
+//         street,
+//         city,
+//         zipcode
+//     };
+// }
+// let createAddress = address('Lê Văn Việt','HCMC',70000);
+//Constructor Function
+function Address(street,city,zipcode){
+    this.street = street;
+    this.city = city;
+    this.zipcode = zipcode;
+    // this.showAddress= function(){
+    //     console.log('Street:', this.street),
+    //     console.log('City', this.city),
+    //     console.log('Zip Code', this.zipcode)
+    // }
+}
+// let createAddress = new Address('Lê Văn Việt','HCMC',70000);
+// console.log(createAddress);
+
+//Ex3
+// let createAddress = new Address('Lê Văn Việt','HCMC',70000);
+// let createAddress2 = new Address('Lê Văn Việt', 'HCMC', 70000);
+// function areEqual(createAddress,createAddress2){
+//     return createAddress.street === createAddress2.street &&
+//     createAddress.city === createAddress2.city &&
+//     createAddress.zipcode === createAddress2.zipcode
+// }
+// function areSame(createAddress,createAddress2){
+//     if (createAddress === createAddress2) return true;
+//     else return false;
+// }
+// const x1 = areEqual(createAddress,createAddress2);
+// const x2 = areSame(createAddress,createAddress2);
+// console.log(x1,x2);
+
+//Ex4
+// const blogPost = {
+//     title: '4:30 AM',
+//     body: 'This is an amazing book',
+//     author: 'XiaJun',
+//     views: 1330,
+//     comments: [
+//         {author: 'Nam', body: 'Great!!!'},
+//         {author: 'Anda', body: 'WOW!!!'}
+//     ],
+//     isLive: true
+// }
+// console.log(blogPost);
+
+//Ex5
+// function Blog(title,body,author){
+//     this.title = title,
+//     this.body = body,
+//     this.author = author,
+//     this.views = 0,
+//     this.comments = [],
+//     this.isLive = false,
+//     this.isPublished = function(){
+//         if(isLive) console.log('This book was published');
+//         else console.log('This book was not published');
+//     }
+// }
+
+//Ex6
